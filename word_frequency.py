997a48b2a9b09ff277bd36423c6818e6c5c2faf3
Greg Sronce <greg.sronce@gmail.com>
2021-06-15 11:36:53 -0400

    Reading file, lowercase, split.

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,5 @@
 import string
 
-import random
-
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
@@ -10,48 +8,41 @@ STOP_WORDS = [
 
 
 class FileReader:
-    def __init__(self, filename='one-today.txt'):
+    def __init__(self, filename):
         self.filename = filename
 
     def read_contents(self):
         """
         This should read all the contents of the file
-        and return them as one string, lowercasing the  
-        text and get all the words from the text.
+        and return them as one string.
         """
         with open(self.filename) as file:
-            text = file.read().lower()
-            return text
-
-
-file = FileReader()
-print(file.read_contents())
+            text = file.read().lower().split()
+        return text
 
 
 class WordList:
     def __init__(self, text):
         self.text = text
 
-    def remove_punctuation(self, text):
+    def remove_punctuation(self):
         """
         This method
         is responsible for stripping
         words of punctuation.
         """
-        new_text_string = ""
-        punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
-        for word in self.text:
-            if word not in punctuation:
-                new_text_string += word
-        return new_text_string
-    
+        self.remove_punct = [word.strip(string.punctuation) for word in self.text]
+        return self.remove_punct
+
     def remove_stop_words(self):
         """
         Removes all stop words from our word list. Expected to
         be run after extract_words.
         """
-        # words_to_count = []
-        # for word in file
+        words_to_count = []
+        for word in self.remove_punct:
+            if word not in STOP_WORDS:
+                words_to_count.append(word)
 
     def get_freqs(self):
         """
@@ -61,10 +52,6 @@ class WordList:
         could be a dictionary or another type of object.
         """
 
-list_of_words = WordList()
-print(list_of_words.remove_punctuation())
-# # print(list_of_words.remove_stop_words())
-# # print(list_of_words.get_freqs())
 
 class FreqPrinter:
     def __init__(self, freqs):
@@ -87,7 +74,6 @@ class FreqPrinter:
        rights | 6    ******
         right | 6    ******
         """
-        raise NotImplementedError("FreqPrinter.print_freqs")
 
 
 if __name__ == "__main__":
@@ -104,7 +90,7 @@ if __name__ == "__main__":
     if file.is_file():
         reader = FileReader(file)
         word_list = WordList(reader.read_contents())
-        word_list.extract_words()
+        word_list.remove_punctuation()
         word_list.remove_stop_words()
         printer = FreqPrinter(word_list.get_freqs())
         printer.print_freqs()
